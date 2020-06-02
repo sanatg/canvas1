97d4b983b43e3a17048b5028f536e0d6e7f2fb59
sanatg <62789975+sanatg@users.noreply.github.com>
2020-06-02 08:33:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var database;
+ 
+var drawing = [];
+function setup(){
+canvas = createCanvas(700,700);
+}
+function draw(){
+background(0);
+
+if(mouseIsPressed){
+    var point = {
+        x: mouseX,
+        y: mouseY
+    }
+    drawing.push(point);
+}
+beginShape();
+stroke(255);
+strokeWeight(4);
+noFill();
+for (var i = 0; i < drawing.length;i++){
+    vertex(drawing[i].x,drawing[i].y)
+}
+endShape();
+
+
+}
+
